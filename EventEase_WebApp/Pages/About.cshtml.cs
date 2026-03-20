8bf9707cdca98cb08e00d36ea7f3b7d845761b18
Loyiso Tose <tosloy01@csgicorp.com>
2026-03-20 10:14:31 +0200

    Create EventEase Admin MVC project

---
+++ EventEase_WebApp/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventEase_WebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
